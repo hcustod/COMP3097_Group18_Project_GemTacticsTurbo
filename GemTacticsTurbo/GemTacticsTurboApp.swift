028d10f5daa2759accb5435fab0b86d7b23f5be3
hcustod <h.custodio.dev@gmail.com>
2026-03-26 00:22:32 -0400

    Initial Commit

---
+++ GemTacticsTurbo/GemTacticsTurboApp.swift
@@ -0,0 +1,17 @@
+//
+//  GemTacticsTurboApp.swift
+//  GemTacticsTurbo
+//
+//  Created by Henrique Custodio on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct GemTacticsTurboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
